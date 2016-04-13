d9d2bad78efc690d354d7086196583fceb3ddff3
Kevin Rogers <kevinrogers@Kevins-iMac.local>
2016-04-12 19:20:14 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+// Load plugins
+var gulp = require('gulp'),
+	plugins = require('gulp-load-plugins')({ camelize: true }),
+  less = require('gulp-less'),
+	lr = require('tiny-lr'),
+	server = lr();
+
+// Styles
+gulp.task('less', function() {
+  return gulp.src('wp-content/themes/noise-wp/less/*.less')
+	.pipe(gulp.dest('wp-content/themes/noise-wp/style.css'))
+	.pipe(plugins.livereload(server))
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Listen on port 35729
+  server.listen(35729, function (err) {
+	if (err) {
+	  return console.log(err)
+	};
+
+	// Watch .scss files
+	gulp.watch('wp-content/themes/noise-wp/less/*.less', ['less']);
+
+  });
+
+});
+
+// Default task
+gulp.task('default', ['less', 'watch']);
